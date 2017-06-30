7fec0af00272974b61c50367f83d3dd42d385922
Milo <msk12345@gmail.com>
2017-07-01 02:16:34 +0800

    优化路径点

--- Map.lua
+++ Map.lua
@@ -96,11 +96,36 @@ function Map:worldCoordinates(tilePos)
   }
 end
 
+function Map:improvePath(path)
+  if #path < 3 or not path then return path end
+  
+  local improvedPath = {}
+  local direction = nil
+  for index = 1, #path - 1 do
+    local nextDirection = {}
+    nextDirection.x = path[index].x - path[index+1].x
+    nextDirection.y = path[index].y - path[index+1].y
+    if not direction then
+      table.insert(improvedPath, path[index])
+    else 
+      if direction.x ~= nextDirection.x or direction.y ~= nextDirection.y then
+        table.insert(improvedPath, path[index])
+      end
+    end
+    direction = nextDirection
+  end
+  if not (improvedPath[#improvedPath].x == path[#path].x and improvedPath[#improvedPath].y == path[#path].y) then
+    table.insert(improvedPath, path[#path])
+  end
+  return improvedPath
+end
+
 function Map:findPath(from, to)
   local tileFrom = self:tileCoordinates(from)
   local tileTo = self:tileCoordinates(to)
   local time = love.timer.getTime()
-	local path = self.pathFinder:getPath(tileFrom.x, tileFrom.y, tileTo.x, tileTo.y)
+	local path = self.pathFinder:getPath(tileFrom.x, tileFrom.y, tileTo.x, tileTo.y, false)
+  path = self:improvePath(path)
   local usage = love.timer.getTime()-time
   self.findPathUsage = self.findPathUsage + usage
   return path
