04d0e2f74dc3f796205c167f042ef34173cd5437
Milo <msk12345@gmail.com>
2017-07-07 02:02:52 +0800

    更新到 0.10.1

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
-    t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
+    t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
     t.console = true                  -- Attach a console (boolean, Windows only)
 
     t.window.title = "Railgun"        -- The window title (string)
