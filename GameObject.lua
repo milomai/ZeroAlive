6eeed8f8a276b1a3defbcd7863b2b5405694888c
Milo <msk12345@gmail.com>
2017-07-20 00:25:51 +0800

    抽象基类

---
+++ GameObject.lua
@@ -0,0 +1,13 @@
+GameObject = class("GameObject", 
+  {
+    pos = {x = 0, y = 0},
+  })
+
+function GameObject:init()
+end
+
+function GameObject:update(dt)
+end
+  
+function GameObject:draw()
+end
