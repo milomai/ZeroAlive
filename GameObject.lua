fe0549c352ebf052e5f781b8c8c0803383802c19
Milo <msk12345@gmail.com>
2017-07-21 21:05:01 +0800

    GameObject 实现 init 和 update 函数

--- GameObject.lua
+++ GameObject.lua
@@ -1,12 +1,24 @@
 GameObject = class("GameObject", 
   {
+    size = 10,
     pos = {x = 0, y = 0},
+    physic = {}
   })
 
-function GameObject:init()
+function GameObject:init(posX, posY)
+  self.pos.x = posX or self.pos.x
+  self.pos.y = posY or self.pos.y
+  self.physic.shape = love.physics.newCircleShape(self.size)
+  self.physic.body = love.physics.newBody(world.physics, self.pos.x, self.pos.y, "dynamic")
+  self.physic.fixture = love.physics.newFixture(self.physic.body, self.physic.shape)
+  self.physic.fixture:setUserData(self)
 end
 
 function GameObject:update(dt)
+  if self.physic.body then
+    self.pos.x = self.physic.body:getX()
+    self.pos.y = self.physic.body:getY()
+  end
 end
   
 function GameObject:draw()
